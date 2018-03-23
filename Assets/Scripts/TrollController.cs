b7000101daca8e0e290492a056e15237fea2a046
Kinqston <vanek-cs@mail.ru>
2018-03-23 14:10:05 +1000

    lvl_1

---
+++ Assets/Scripts/TrollController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TrollController : MonoBehaviour {
+
+    NavMeshAgent _troll;
+    public Transform castle;
+    // Use this for initialization
+    void Start()
+    {
+        _troll = GetComponent<NavMeshAgent>();
+        _troll.SetDestination(castle.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
