06327c42f9e4feefdc8f58ca8c2f19f15dd8dfe3
Kinqston <vanek-cs@mail.ru>
2018-03-27 19:14:14 +1000

    mainmenu

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour {
 
-    public Transform target;
+    private Transform target;
     public float speed;
 
     public void Rage(Transform _target) {
