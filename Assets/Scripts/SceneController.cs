889ee43d3a53ad01e4d9b12875d6349eddebb156
Kinqston <vanek-cs@mail.ru>
2018-03-27 19:59:21 +1000

    perehod po scenam

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+	// Use this for initialization
+	public void Scene_Controller(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
