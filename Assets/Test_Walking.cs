57d543a29f640574fe886cc8469bc2da8aafb2b1
Kinqston <vanek-cs@mail.ru>
2018-05-10 13:43:33 +1000

    3 trolls

---
+++ Assets/Test_Walking.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Walking : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.Translate(0,0,Time.deltaTime);
+        if(gameObject.transform.position.z > 4)
+        gameObject.GetComponent<Animator>().SetBool("Dead",true);
+	}
+}
